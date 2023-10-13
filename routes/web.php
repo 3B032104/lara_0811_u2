13416e7e1a5855dcc7fc1a8557b50863f424c8e8
3B032104 <3b032104@gm.student.ncut.edu.tw>
2023-10-13 11:21:45 +0800

    Test route retrun string welcome

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route retrun string 'welcome'
+Route::get('/', function () {
+    return 'welcome';
+});
